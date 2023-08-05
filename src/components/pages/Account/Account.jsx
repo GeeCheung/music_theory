aa49608e4311671b60beebeaefc1732d8ebf9cbd
Gee Cheung <gcheung16@hotmail.com>
2023-08-05 13:54:36 +0100

    folder reorder

--- src/components/pages/Account/Account.jsx
+++ src/components/pages/Account/Account.jsx
@@ -1,14 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { db } from "../../firebase-config";
-import StoreChords from "../Chords/Chords";
-import DisplayValueBox from "../DisplayValue.js";
-import Footer from "../Footer/Footer";
-import Topnavbar from "../Navbars/TopNavbar";
-import StoreNote from "../Notes/Note.js";
-import StoreScales from "../Scales/Scales";
-import Pianotokey from "../Visual_Piano/Piano_2";
+import { db } from "../../../firebase-config";
+import StoreChords from "../../Chords/Chords";
+import DisplayValueBox from "../../DisplayValue.js";
+import Footer from "../../Footer/Footer";
+import Topnavbar from "../../Navbars/TopNavbar";
+import StoreNote from "../../Notes/Note.js";
+import StoreScales from "../../Scales/Scales";
+import Pianotokey from "../../Visual_Piano/Piano_2";
 
 const Account = () => {
   const [musictheory, setMusictheory] = useState([]);
